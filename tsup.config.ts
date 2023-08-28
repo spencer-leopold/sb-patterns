70ce947c12b3038dc3f20ffbd8191e150e1057b9
Spencer Leopold <spencer.leopold@gmail.com>
2023-08-28 14:16:29 -0400

    Initial commit.

---
+++ tsup.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig((options) => ({
+  entry: ["src/index.ts"],
+  splitting: false,
+  minify: !options.watch,
+  format: ["cjs", "esm"],
+  dts: {
+    resolve: true,
+  },
+  treeshake: true,
+  sourcemap: true,
+  clean: true,
+  esbuildOptions(options) {
+    options.conditions = ["module"];
+  },
+}));
